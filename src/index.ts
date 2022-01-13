4bf0cf913c552271ebb1e7c82cce61f9fc631d41
matmoimjs <matmoimjs@gmail.com>
2022-01-13 07:26:09 +0200

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,29 @@
 import Redis from 'ioredis';
 import dotenv from 'dotenv';
-import TPubSub, { PubSubClient, PubSubFromServer, PubSubFromWs, } from '@nik-kita/kucoin-777-pub-sub-types';
 import { KucoinWebsocket } from './kucoin-websocket';
+import { PubSubChannel, PubSubToWs } from '@nik-kita/kucoin-777-pub-sub-types';
 
+const { CLOSE, OPEN, SUBSCRIBE, UNSUBSCRIBE } = PubSubToWs;
+let ku: KucoinWebsocket | null = null;
 dotenv.config();
+
 (async () => {
     const pub = new Redis({ db: 7 });
     const sub = pub.duplicate();
 
-    await sub.subscribe('to:ws:subscribe', 'to:ws:unsubscribe');
+    await sub.subscribe(PubSubChannel.TO_WS);
 
-    sub.on('message', (channel, message) => {
-        if (channel !== 'to:ws' && message !== 'open') return;
+    sub.on('message', (channel, data) => {
+        const { _, ...message } = JSON.parse(data);
 
-        KucoinWebsocket.open(pub)
-            .then((kucoinWebsocket) => {
-                sub.on('message', (channel2, message2) => {
-                    if (channel2 === 'to:ws:subscribe') {
-                        const { subject, data } = JSON.parse(message2);
-                        kucoinWebsocket.subscribe(subject)(data);
-                    } else if (channel2 === 'to:ws:unsubscribe') {
-                        kucoinWebsocket.unsubscribe(JSON.parse(message2));
-                    } else if (channel2 === 'to:ws') {
-                        if (message2 === 'close') {
-                            kucoinWebsocket.close();
-                        }
-                    }
-                });
-            });
+        if (_ === SUBSCRIBE) {
+            ku?.subscribe(message.subject)(message.payload);
+        } else if (_ === UNSUBSCRIBE) {
+            ku?.unsubscribe(message.subject)(message.payload);
+        } else if (_ === OPEN) {
+            KucoinWebsocket.open(pub).then((kucoinWebSocket) => ku = kucoinWebSocket);
+        } else if (_ === CLOSE) {
+            ku?.close();
+        }
     });
 })();
